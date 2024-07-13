809fcb6ece65f01466c00dcf36adaef59341312f
渡邊 英里香 <a5821101@aoyama.jp>
2024-07-14 00:51:21 +0900

    量子アニーリングでトラッキングエラー最小化

--- quantum_ver2.py
+++ quantum_ver2.py
@@ -1,25 +1,9 @@
-# # 目的関数の作成
-# f = q[0] * q[1] + q[0] -q[1] + 1
-# print(f)
-
-# from amplify import FixstarsClient
-# client = FixstarsClient()
-# client.token = "AE/VfQDHqAtq9NOTUnJyxWiDTSGa7avMJQe" 
-# client.parameters.timeout = 1000
-# from amplify import solve
-# result = solve(f, client)
-
-# print(result.best.values)
-# print(result.best.objective)
-# print(f"{q} = {q.evaluate(result.best.values)}")
 
 # 1. 変数の初期設定等
 from amplify import VariableGenerator
 gen = VariableGenerator()
 q = gen.array("Binary", 2146) # 二値変数
-Cardi = 10 # カーディナリティ制約
-
-
+Cardi = 100 # カーディナリティ制約
 
 
 
@@ -41,8 +25,6 @@ for i in range(len(lst)):
 
 
 
-
-
 # 3. 銘柄、購入数の決定
 # 3. 1. Jquantsから株価データを取得
 import requests
@@ -57,8 +39,8 @@ headers = {'Authorization': 'Bearer {}'.format(idToken)}
 
 # 3. 2. time_pointを先に取得
 time_point = []
-from_ = "2022-04-19" # 取得できる期間変わるので定期的に更新しないと
-to_ = "2023-03-19"
+from_ = "2022-04-21" # 取得できる期間変わるので定期的に更新しないと
+to_ = "2023-03-21"
 code_ = "7203"
 url = "https://api.jquants.com/v1/prices/daily_quotes"
 res = requests.get(f"{url}?code={code_}&from={from_}&to={to_}", headers=headers)
@@ -89,7 +71,7 @@ for key in monthly_data.keys():
     date_first = monthly_data[key][0]
     date_last = monthly_data[key][-1]    
 
-    for i in range(10):
+    for i in range(Cardi):
     # for i in range(len(code_2146)):
         res_first = requests.get(f"{url}?code={code_2146[i]}&date={date_first}", headers=headers)
         res_last = requests.get(f"{url}?code={code_2146[i]}&date={date_last}", headers=headers)
@@ -98,12 +80,8 @@ for key in monthly_data.keys():
         data_close_first[key].append(data_first["daily_quotes"][0]["Close"])
         data_close_last[key].append(data_last["daily_quotes"][0]["Close"])
         # 銘柄コードはcode_2146[i]
-    print(key, "の株価 : ", data_close_first[key], data_close_last[key])
+    # print(key, "の株価 : ", data_close_first[key], data_close_last[key])
 
-
-end_time = time.time()
-execution_time = end_time - start_time
-print(f"実行時間: {execution_time}秒")
 
 
 # 3. 4. TOPIXの株価取得
@@ -113,14 +91,26 @@ import pandas as pd
 point_topix = []
 
 source = 'stooq'
-dt_s = date(2022, 4, 18)
-dt_e = date(2023, 3, 18)
+dt_s = date(2022, 4, 21)
+dt_e = date(2023, 3, 21)
 symbol = '^TPX'
 df_topix = web.DataReader(symbol, source, dt_s, dt_e)
 df_topix = df_topix.sort_values("Date").reset_index()
+# print(df_topix.loc[df_topix['Date'] == '2022-04-18', 'Close'].values[0])
 
 for i in range(len(df_topix)):
     point_topix.append(df_topix.at[i, "Close"])
+
+topix_first = defaultdict(list)
+topix_last = defaultdict(list)
+# 月初・月末のDateの時のCloseを取得
+for key in monthly_data.keys():
+    date_first = monthly_data[key][0]
+    date_last = monthly_data[key][-1]
+    topix_first[key].append(df_topix.loc[df_topix['Date'] == date_first, 'Close'].values[0])
+    topix_last[key].append(df_topix.loc[df_topix['Date'] == date_last, 'Close'].values[0])
+    # print(key, "の月初値", topix_first[key])
+    # print(key, "の月末値", topix_last[key])
 
 
 
@@ -128,8 +118,49 @@ for i in range(len(df_topix)):
 # 4. 量子アニーリングで組み入れ銘柄決定
 # 4. 1. 目的関数の生成
 object_f = 0
-over_return = 0
+over_return = []
+
+# 4. 2. 超過リターンの計算
+import numpy as np
+import math
 for key in monthly_data.keys():
-    for i in range(len(lst)):
-        portpholio_return = (data_close_last[key][i] - data_close_first[key][i]) / data_close_first[key][i]
-        portpholio_return = (data_close_last[key][i] - data_close_first[key][i]) / data_close_first[key][i]
+    topix_return = (np.array(topix_last[key]) - np.array(topix_first[key])) / np.array(topix_first[key])
+    portpholio_return = 0
+    for i in range(Cardi):
+        # ここで二値変数q[i]をかける！
+        portpholio_return = portpholio_return + (data_close_last[key][i] - data_close_first[key][i]) * q[i] / data_close_first[key][i]
+    over_return.append(portpholio_return - topix_return)
+
+over_return_ave = np.mean(over_return)
+# print(over_return_ave)
+
+mult = 0
+for i in range(len(over_return)):
+    mult = mult + (over_return[i] - over_return_ave) ** 2
+f = mult[0]
+print(f)
+
+# object_f = math.sqrt(mult / (Cardi - 1))
+# print(object_f)
+
+# 目的関数にルート入れるとバグる、分散の最小化でもいいのかしら
+
+
+
+from amplify import FixstarsClient
+client = FixstarsClient()
+client.token = "AE/VfQDHqAtq9NOTUnJyxWiDTSGa7avMJQe" 
+client.parameters.timeout = 1000
+from amplify import solve
+result = solve(f, client)
+
+print(result.best.values)
+print(result.best.objective)
+print(f"{q} = {q.evaluate(result.best.values)}")
+print("トラッキングエラー : ", math.sqrt(result.best.objective) * 100)
+
+
+# 実行時間表示
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"実行時間: {execution_time}秒")
